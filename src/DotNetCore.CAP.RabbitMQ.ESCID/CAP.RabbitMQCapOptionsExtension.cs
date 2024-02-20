4ecfa460c48fbf99315c3b8cd6548af7fd91aab2
Mantas Kareiva <m.kareiva@cid.com>
2024-02-20 12:15:32 +0200

    Adding support for ESP Rabbit library for publishing

---
+++ src/DotNetCore.CAP.RabbitMQ.ESCID/CAP.RabbitMQCapOptionsExtension.cs
@@ -0,0 +1,60 @@
+﻿// Copyright (c) .NET Core Community. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+using System;
+using System.Reflection;
+using DotNetCore.CAP.RabbitMQ;
+using DotNetCore.CAP.Transport;
+using ESCID.ESP.Messaging.RabbitMQ;
+using Microsoft.Extensions.DependencyInjection;
+
+// ReSharper disable once CheckNamespace
+namespace DotNetCore.CAP
+{
+    internal sealed class InjectedContractAssemblies
+    {
+        public Assembly[] Assemblies { get; set; }
+    }
+
+    internal sealed class RabbitMQCapOptionsExtension : ICapOptionsExtension
+    {
+        private readonly Action<RabbitMQOptions> _configure;
+        private readonly RabbitMQOptions _options;
+
+        public RabbitMQCapOptionsExtension(Action<RabbitMQOptions> configure)
+        {
+            _options = new RabbitMQOptions();
+            configure(_options);
+            _configure = configure;
+        }
+
+        public void AddServices(IServiceCollection services)
+        {
+            var rmqConnection = new RabbitMqConnection
+            {
+                UserName = _options.UserName,
+                Password = _options.Password,
+                HostNames = _options.HostName.Split(new[] { ',' }, StringSplitOptions.RemoveEmptyEntries),
+                VirtualHost = "/",
+            };
+
+            var assemblies = new InjectedContractAssemblies
+            {
+                Assemblies = _options.InjectedContractsAssemblies
+            };
+
+            services.AddSingleton(assemblies);
+            services.AddSingleton(rmqConnection);
+            services.AddSingleton<CapMessageQueueMakerService>();
+             
+            services.Configure(_configure);
+
+            // ESCID
+            services.AddRabbitMQ(rmqConnection)
+                .EnablePublisherTransactions();
+
+            // Original CAP
+            services.AddSingleton<ITransport, RabbitMQTransport>();
+        }
+    }
+}
---
+++ src/DotNetCore.CAP.RabbitMQ.ESCID/ITransport.RabbitMQ.cs
@@ -0,0 +1,83 @@
+﻿// Copyright (c) .NET Core Community. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+using DotNetCore.CAP.Internal;
+using DotNetCore.CAP.Messages;
+using DotNetCore.CAP.Transport;
+using ESCID.ESP.Messaging.RabbitMQ;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNetCore.CAP.RabbitMQ
+{
+    internal sealed class RabbitMQTransport : ITransport
+    {
+        private readonly IRabbitMqPublisher _rabbitMqPublisher;
+        private readonly ILogger _logger;
+        private readonly RabbitMqConnection _rabbitMqConnection;
+        private readonly InjectedContractAssemblies _injectedContractAssemblies;
+
+        public RabbitMQTransport(
+            ILogger<RabbitMQTransport> logger,
+            IRabbitMqPublisher rabbitMqPublisher,
+            RabbitMqConnection rabbitMqConnection,
+            InjectedContractAssemblies injectedContractAssemblies)
+        {
+            _logger = logger;
+            _rabbitMqPublisher = rabbitMqPublisher;
+            _rabbitMqConnection = rabbitMqConnection;
+            _injectedContractAssemblies = injectedContractAssemblies;
+        }
+
+        public BrokerAddress BrokerAddress => new BrokerAddress("RabbitMQ", string.Join(",", _rabbitMqConnection.HostNames));
+
+        public Task<OperateResult> SendAsync(TransportMessage message)
+        {
+            try
+            {
+                string json = Encoding.UTF8.GetString(message.Body);
+                string messageFullName = message.GetName();
+                Type messageType = _injectedContractAssemblies.Assemblies
+                    .SelectMany(x => x.GetTypes())
+                    .FirstOrDefault(x => x.FullName == messageFullName);
+                if (messageType == null)
+                    throw new InvalidOperationException($"Type '{messageFullName}' was not found.");
+
+                MethodInfo methodDeserialize = Array.Find(typeof(JsonConvert).GetMethods(), x => x.Name == "DeserializeObject" && x.IsGenericMethod && x.GetParameters().Length == 1);
+
+                if (methodDeserialize == null)
+                    throw new InvalidOperationException("DeserializeObject method not found.");
+
+                MethodInfo methodDeserializeGeneric = methodDeserialize.MakeGenericMethod(messageType);
+                var typedMessagePayload = methodDeserializeGeneric.Invoke(null, new object[] { json });
+
+                MethodInfo methodPublish = _rabbitMqPublisher.GetType().GetMethod("Publish");
+                if (methodPublish == null)
+                    throw new InvalidOperationException("Publish method not found.");
+
+                MethodInfo methodPublishGeneric = methodPublish.MakeGenericMethod(messageType);
+                methodPublishGeneric.Invoke(_rabbitMqPublisher, new object[] { typedMessagePayload, null });
+
+                _logger.LogDebug($"RabbitMQ topic message [{message.GetName()}] has been published.");
+
+                return Task.FromResult(OperateResult.Success);
+            }
+            catch (Exception ex)
+            {
+                var wrapperEx = new PublisherSentFailedException(ex.Message, ex);
+                var errors = new OperateError
+                {
+                    Code = ex.HResult.ToString(),
+                    Description = ex.Message
+                };
+
+                return Task.FromResult(OperateResult.Failed(wrapperEx, errors));
+            }
+        }
+    }
+}
